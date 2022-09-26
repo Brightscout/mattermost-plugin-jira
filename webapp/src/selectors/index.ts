8e7fad4f88149bf41e3389d655bcbd21c1e12bc2
Nityanand Rai <nityanand.rai@brightscout.com>
2022-09-26 17:44:09 +0530

    Review fixes done

--- webapp/src/selectors/index.ts
+++ webapp/src/selectors/index.ts
@@ -13,21 +13,14 @@ const getPluginState = (state) => state['plugins-' + PluginId] || {};
 
 export const getPluginServerRoute = (state) => {
     const config = getConfig(state);
-    let basePath = '';
-    let url;
+    let baseUrl = config?.configSiteURL ? config.SiteUrl : window.location.href;
+    let basePath = new URL(baseUrl).pathname;
 
-    if (config && config.SiteURL) {
-        url = new URL(config.SiteURL);
-    } else {
-        url = new URL(window.location.href);
-    }
-
-    basePath = url.pathname;
     if (basePath && basePath[basePath.length - 1] === '/') {
         basePath = basePath.substr(0, basePath.length - 1);
     }
 
-    return `${url.origin}${basePath}/plugins/${PluginId}`;
+    return `${new URL(baseUrl).origin}${basePath}/plugins/${PluginId}`;
 };
 
 export const getCurrentUserLocale = createSelector(
